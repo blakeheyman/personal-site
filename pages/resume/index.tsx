0ec0c66fef7673e06a60427bc2e165dff3797290
Blake Heyman <blake.heyman@gmail.com>
2022-11-23 20:33:45 -0500

    Resume

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,8 +1,21 @@
-export default function Resume() {
-    // TODO
-    return (
-        <div>
+import { Grid, Text } from "@nextui-org/react";
 
-        </div>
-    )
+export default function Resume() {
+  return (
+    <div style={{ margin: "0 20px" }}>
+      <Text h1 style={{ textAlign: "center" }}>
+        My Resume
+      </Text>
+
+      <Grid.Container justify="center">
+        <Grid xs={12} md={8}>
+          <iframe
+            width="100%"
+            src="BlakeHeymanResume.pdf#toolbar=0&view=FitH"
+            style={{ aspectRatio: 8.5 / 11 }}
+          />
+        </Grid>
+      </Grid.Container>
+    </div>
+  );
 }
