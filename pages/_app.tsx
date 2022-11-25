9c66fda6227ab62145f5f4c3d2114de9c86dda29
Blake Heyman <blake.heyman@gmail.com>
2022-11-24 22:00:38 -0700

    Add HTML heads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 import NavBar from "../components/NavBar";
+import Head from "next/head";
 
 const theme = createTheme({
   type: "light",
@@ -19,9 +20,21 @@ const theme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider theme={theme}>
-      <NavBar />
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <Head>
+        <title>Blake Heyman - Mobile App Developer</title>
+        <meta
+          property="og:title"
+          content="Blake Heyman - Mobile App Developer"
+          key="title"
+        />
+        <meta name="keywords" content="iOS, Android, mobile apps, web" />
+        <meta name="author" content="Blake Heyman" />
+      </Head>
+      <NextUIProvider theme={theme}>
+        <NavBar />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
   );
 }
