f2d98ec1377fd01a9dd919f2b0d637702a5edb76
Blake Heyman <blake.heyman@gmail.com>
2025-07-20 23:38:14 -0400

    Fix linting error

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -16,21 +16,19 @@ export default function About() {
       <div className="flex flex-col flex-[2] p-16 lg:p-40">
         <h1 className="font-bold text-4xl mb-6">About Me</h1>
         <p className="font-normal text-xl leading-relaxed">
-          I'm currently a web and mobile developer at Wellframe, where I've been
-          building tools that help people navigate their healthcare more easily
-          and effectively. I graduated from Northeastern University in 2023 with
-          a degree in computer science, concentrating in human-centered
-          computing, an approach I bring to work every day.
+          {
+            "I'm currently a web and mobile developer at Wellframe, where I've been building tools that help people navigate their healthcare more easily and effectively. I graduated from Northeastern University in 2023 with a degree in computer science, concentrating in human-centered computing, an approach I bring to work every day."
+          }
           <br />
           <br />
-          I'm a problem-solver at heart, and I see coding as a powerful way to
-          support and empower others. I'm especially passionate about creating
-          intuitive, accessible experiences through thoughtful design and
-          technology.
+          {
+            "I'm a problem-solver at heart, and I see coding as a powerful way to support and empower others. I'm especially passionate about creating intuitive, accessible experiences through thoughtful design and technology."
+          }
           <br />
           <br />
-          Outside of work, you'll usually find me baking, thrifting, or playing
-          on my 3DS. I also love exploring new restaurants around Boston.
+          {
+            "Outside of work, you'll usually find me baking, thrifting, or playing on my 3DS. I also love exploring new restaurants around Boston."
+          }
         </p>
       </div>
     </div>
